760118902ebfdfbc27ffb7734197d52aa9dd795f
Simon Pfreundschuh <simonpf@chalmers.se>
2016-04-27 08:37:25 +0200

    Initial site commit.

---
+++ blog/about.md
@@ -0,0 +1,2 @@
+This blog serves to document my work for the 2016 Google Summer of Code
+project GPU-accelerated deep neural network in TMVA.
---
+++ blog/site.hs
@@ -0,0 +1,63 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+import           Hakyll.Core.Configuration
+
+
+--------------------------------------------------------------------------------
+conf :: Configuration
+conf = defaultConfiguration
+  {
+  destinationDirectory = "./blog/_site",
+  storeDirectory       = "./blog/_cache",
+  tmpDirectory         = "./blog/_cache/tmp",
+  providerDirectory    = "./blog"
+  }
+
+main :: IO ()
+main = hakyllWith conf $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match (fromList ["index.md", "about.md", "contact.markdown"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+    match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
