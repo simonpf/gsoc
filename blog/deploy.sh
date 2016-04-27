f98fa148508198fc7f6516eb04c30b2250ba5082
Simon Pfreundschuh <simonpf@chalmers.se>
2016-04-27 21:09:17 +0200

    deploy.sh update.

--- blog/deploy.sh
+++ blog/deploy.sh
@@ -4,7 +4,7 @@ git stash
 git checkout gh-pages
 
 cabal run site rebuild
-cp -r blog/_site/* .
+cp -r ./blog/_site/* .
 git add *.html ./css ./images ./posts
 git commit -am "Blog update."
 git push
